7417ec2e5e8a48fcc4a739ceea908ef46daa4404
UniqueoneZ <124648060+UniqueoneZ@users.noreply.github.com>
2024-07-27 13:45:12 +0800

    Update run_main.sh

--- run_main.sh
+++ run_main.sh
@@ -1,6 +1,6 @@
+sh scripts/MSegRNN/weather.sh;
 sh scripts/MSegRNN/electricity.sh;
 sh scripts/MSegRNN/etth1.sh;
 sh scripts/MSegRNN/etth2.sh;
 sh scripts/MSegRNN/ettm1.sh;
 sh scripts/MSegRNN/ettm2.sh;
-sh scripts/MSegRNN/weather.sh;

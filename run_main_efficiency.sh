947a999a61ea623d6b27816e354ddb25fded2922
UniqueoneZ <124648060+UniqueoneZ@users.noreply.github.com>
2024-07-30 18:23:01 +0800

    Add files via upload

---
+++ run_main_efficiency.sh
@@ -0,0 +1,5 @@
+sh scripts/ISMRNN/model_efficiency/iTransformer_weather.sh;
+sh scripts/ISMRNN/model_efficiency/DLinear_weather.sh
+sh scripts/ISMRNN/model_efficiency/ISMRNN_weather.sh
+sh scripts/ISMRNN/model_efficiency/PatchTST_weather.sh;
+sh scripts/ISMRNN/model_efficiency/SegRNN_weather.sh;

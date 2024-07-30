f38fa58b23a841e17a41f78c08c99cc16493a761
UniqueoneZ <124648060+UniqueoneZ@users.noreply.github.com>
2024-07-30 16:08:00 +0800

    Update run_main.sh

--- run_main.sh
+++ run_main.sh
@@ -1,6 +1,7 @@
+sh scripts/ISMRNN/weather.sh;
 sh scripts/ISMRNN/electricity.sh;
 sh scripts/ISMRNN/etth1.sh;
 sh scripts/ISMRNN/etth2.sh;
 sh scripts/ISMRNN/ettm1.sh;
 sh scripts/ISMRNN/ettm2.sh;
-sh scripts/ISMRNN/weather.sh;
+
